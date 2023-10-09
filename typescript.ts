3b2134b42dd4d153324c3319ff2d89b0ebf06304
Myriam <myriam@ctoutvert.com>
2023-10-09 10:38:54 +0200

    test

---
+++ typescript.ts
@@ -0,0 +1,19 @@
+let message: string = 'Hello, World!'; 
+console.log(message);
+
+
+function calcAdd(a: number, b: number){
+    return a+b;
+}
+
+function calcSoustr(a: number, b: number){
+    return a-b;    
+}
+
+function calcMult(a: number, b: number){
+    return a*b;    
+}
+
+function calcDiv(a: number, b: number){
+    return a/b;
+}
