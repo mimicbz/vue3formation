3d376abaeca5b7ad6e63a603aea86f8b0a6ad576
Myriam <myriam@ctoutvert.com>
2023-10-09 10:54:53 +0200

    Création Calculatrice

---
+++ ts/typescript.js
@@ -0,0 +1,18 @@
+/*let message: string = 'Hello, World!';
+console.log(message);*/
+function calcAdd(a, b) {
+    return a + b;
+}
+function calcSoustr(a, b) {
+    return a - b;
+}
+function calcMult(a, b) {
+    return a * b;
+}
+function calcDiv(a, b) {
+    return a / b;
+}
+console.log("Test Addition" + calcAdd(25, 12));
+console.log("Test Soustraction" + calcSoustr(25, 12));
+console.log("Test Multiplication : " + calcMult(25, 12));
+console.log("Test Division" + calcDiv(25, 12));
---
+++ ts/typescript.ts
@@ -0,0 +1,28 @@
+/*let message: string = 'Hello, World!'; 
+console.log(message);*/
+
+
+function calcAdd(a: number, b: number): number{
+    return a+b;
+}
+
+function calcSoustr(a: number, b: number): number{
+    return a-b;    
+}
+
+function calcMult(a: number, b: number): number{
+    return a*b;    
+}
+
+function calcDiv(a: number, b: number): number{
+    return a/b;
+}
+
+console.log("Test Addition" + calcAdd(25, 12));
+
+console.log("Test Soustraction" + calcSoustr(25, 12));
+
+console.log("Test Multiplication : " + calcMult(25, 12));
+
+console.log("Test Division" + calcDiv(25, 12));
+
